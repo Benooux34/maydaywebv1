92d3d2fb503e27f5c71c77793885877b0fc015fe
Benoux34 <benoitantunes34130@gmail.com>
2023-05-17 15:07:14 +0200

    fix hero mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const pd = Playfair_Display({ subsets: ['latin'] })
 
 function Hero() {
   return (
-    <section className="h-[85vh] flex items-center justify-center w-[90%] xl:w-[65%] text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center font-bold">
+    <section className="h-[85vh] flex items-center justify-center w-[95%] xl:w-[65%] text-white text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl text-center font-bold">
         <div className='space-y-2 xl:space-y-5'>
             <h1>Développons ensemble</h1>
             <h1>votre <span className={`text-[#F9C578] ${pd.className}`}>Site Internet</span></h1>
