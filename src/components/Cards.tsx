93807386a3ece6e01ad4aefcffa96f630cd41a3f
Benoux34 <benoitantunes34130@gmail.com>
2023-05-31 22:59:42 +0200

    add card design

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react'
 // Animation
 import { motion, useScroll, useMotionValueEvent } from 'framer-motion'
+import Card from './Card'
 
 function Cards() {
     // Switch Mode
@@ -22,54 +23,58 @@ function Cards() {
     })
 
   return (
-    <section className="h-[200vh] md:h-[70vh] w-full flex items-center justify-center -mt-20 mb-20">
-        <div className="h-full w-full flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-10 lg:space-x-20 space-y-10 md:space-y-0 mx-10 xl:mx-[8%]">
+    <section className="h-[200vh] md:h-[90vh] w-full flex items-center justify-center -mt-24 mb-20">
+        <div className="h-full w-full flex flex-col md:flex-row items-center justify-center space-x-0 md:space-x-10 lg:space-x-20 space-y-10 md:space-y-0 px-10 xl:px-[5%]">
             <motion.div 
-                className="relative bg-[#14787C] h-full w-[90%] md:w-1/3 rounded-xl"
+                className="bg-[#eaeaea] h-[100vh] md:h-[90vh] w-[90vw] md:w-[30vw] rounded-xl shadow-xl"
                 initial={{ y: "12%" }}
             >
-                <div className='absolute top-5 w-full flex items-center justify-between px-5'>
-                    <div className='flex items-center justify-center space-x-3'>
-                        <h2 className='text-left text-white text-2xl md:text-4xl'>Tarif</h2>
-                        <span className={active ? 'text-left text-[#F9C578] text-xl md:text-3xl' : 'hidden'}>GOLD</span>
-                    </div>
-                    <button onClick={() => setActive(!active)} className='text-white text-md'>Switch Mode</button>
-                </div>
-
-                <div className='absolute bottom-20 w-full flex items-center justify-center'>
-                    {active ?
-                        <div className='flex items-center justify-center space-x-4'>
-                            <span className='text-[#F9C578] text-2xl md:text-4xl'>19.99€</span>
-                            <span className='text-white text-lg md:text-xl font-light'>par mois</span>
-                        </div>
-                        :
-                        <div className='flex items-center justify-center space-x-4'>
-                            <span className='text-white text-2xl md:text-4xl'>99.99€</span>
-                            <span className='text-white text-lg md:text-xl font-light'>pour 1 site</span>
-                        </div>
-                    }
-                </div>
-
-                <div className='absolute bottom-5 w-full flex items-center justify-center'>
-                    <button className='bg-transparent hover:bg-[#F9C578] text-[#F9C578] hover:text-[#14787C] text-xl font-semibold border-[3px] border-[#F9C578] px-5 py-2 rounded-lg transition-all'>
-                        En Savoir Plus
-                    </button>
-                </div>
+                <Card
+                    active={active}
+                    setActive={setActive}
+                    name="Start"
+                    description="Le tarif 'Start' est bla bla bla bla, seb sort ta flute"
+                    price="200"
+                    colorText="black"
+                    colorTextHover="white"
+                    colorButton="black"
+                    colorTextButton="black"
+                />
             </motion.div>
                 
             <motion.div 
-                className="bg-[#14787C] h-full w-[90%] md:w-1/3 rounded-xl"
+                className="bg-[#eaeaea] h-[100vh] md:h-[90vh] w-[90vw] md:w-[30vw] rounded-xl shadow-xl"
                 animate={scroll ? { y: "12%" } : { y: count }}
             >
-            
+                <Card
+                    active={active}
+                    setActive={setActive}
+                    name="Premium"
+                    description="Le tarif 'Premium' est bla bla bla bla, seb sort ta flute"
+                    price="500"
+                    colorText="black"
+                    colorTextHover="white"
+                    colorButton="black"
+                    colorTextButton="black"
+                />
             </motion.div>
 
             <motion.div 
-                className="bg-[#14787C] h-full w-[90%] md:w-1/3 rounded-xl"
+                className="bg-[#eaeaea] h-[100vh] md:h-[90vh] w-[90vw] md:w-[30vw] rounded-xl shadow-xl"
                 initial={{ y: "12%" }}
             >
-
-            </motion.div>        
+                <Card
+                    active={active}
+                    setActive={setActive}
+                    name="Advance"
+                    description="Le tarif 'Advance' est bla bla bla bla, seb sort ta flute"
+                    price="1000"
+                    colorText="black"
+                    colorTextHover="white"
+                    colorButton="black"
+                    colorTextButton="black"
+                />
+            </motion.div>     
         </div>
     </section>
   )
