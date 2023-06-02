df13f130235bfacb2bdeec4b86e88cdc6807430e
Benoux34 <benoitantunes34130@gmail.com>
2023-06-03 00:30:50 +0200

    test h-100vh

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,7 +8,7 @@ const pd = Playfair_Display({ subsets: ['latin'] })
 
 function Hero() {
   return (
-    <section className="h-[85vh] flex items-center justify-center w-[95%] xl:w-[65%] text-white text-center font-bold">
+    <section className="h-[100vh] flex items-center justify-center w-[95%] xl:w-[65%] text-white text-center font-bold">
         <div className='space-y-2 xl:space-y-5'>
             <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl'>Développons ensemble</h1>
             <h1 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl pb-3'>votre <span className={`text-[#F9C578] text-3xl sm:text-4xl md:text-5xl lg:text-6xl xl:text-7xl ${pd.className}`}>Site Internet</span></h1>
